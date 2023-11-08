8656bebea18f2d37e2c4fa02c3a7e8d316ebe3bb
Marianela Munoz <marianelamunozgutierrez@gmail.com>
2023-11-08 11:45:02 -0400

    Update post for existing user

--- app.rb
+++ app.rb
@@ -14,13 +14,22 @@ else
   csv_data = CSV.read(csv_file_path, headers: true)
 end
 
+# Function to check if a user already exists
+def user_exists?(csv_data, user_id)
+  user_id = user_id.to_i
+  csv_data.each do |row|
+    return true if row['id'].to_i == user_id
+  end
+  false
+end
+
 # Route to display the list of users
 get '/' do
   @csv_data = csv_data
   erb :index
 end
 
-# Route to add a new user
+# Route to add a new user or update an existing user
 post '/create' do
   new_user = {
     id: params[:id],
@@ -28,42 +37,18 @@ post '/create' do
     status: params[:status]
   }
 
-  CSV.open(csv_file_path, 'a') do |csv|
-    csv << [new_user[:id], new_user[:user], new_user[:status]]
+  if user_exists?(csv_data, new_user[:id])
+    existing_user = csv_data.find { |row| row['id'].to_i == new_user[:id].to_i }
+    existing_user['user'] = new_user[:user]
+    existing_user['status'] = new_user[:status]
+  else
+    csv_data << [new_user[:id], new_user[:user], new_user[:status]]
   end
 
-  csv_data = CSV.read(csv_file_path, headers: true)
+  CSV.open(csv_file_path, 'w') do |csv|
+    csv << csv_data.headers
+    csv_data.each { |row| csv << row }
+  end
 
   redirect '/'
 end
-
-# # Add a new user to the CSV file or update it if it already exists
-
-# def user_exists?(csv_data, user_id)
-#   user_id = user_id.to_i
-#   csv_data.each do |row|
-#     return true if row['id'].to_i == user_id
-#   end
-#   false
-# end
-
-# if user_exists?(csv_data, new_user[:id])
-#   user_id = new_user[:id].to_i
-
-#   # Create a new array with the updated data
-#   updated_data = csv_data.map do |row|
-#     if row['id'].to_i == user_id
-#       [new_user[:id], new_user[:user], new_user[:status]]
-#     else
-#       row.fields
-#     end
-#   end
-
-#   # Replace the contents of the CSV file with the new updated array
-#   CSV.open(csv_file_path, 'w') do |csv|
-#     csv << csv_data.headers
-#     updated_data.each { |row| csv << row }
-#   end
-
-#   puts "User with ID #{new_user[:id]} updated."
-# end
